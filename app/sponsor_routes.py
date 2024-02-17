7bf82702f74a4302a3f5520a75bcc8051a6cf7ba
arronkiptoo <kiptooaarron@gmail.com>
2024-02-18 02:57:13 +0300

    Added flask-restx in sponsor routes to handle swagger documentation

--- app/sponsor_routes.py
+++ app/sponsor_routes.py
@@ -1,12 +1,20 @@
-# resources.py
+from flask_restx import Resource, fields, Namespace, reqparse
 from flask import request
-from flask_restful import Resource
 from models import db, StudentDetails, Bursary
 from serializers import StudentDetailsSchema, BursarySchema
 from marshmallow import ValidationError
 import uuid
 
+api = Namespace('sponsor', description='Sponsor related operations')
+
+bursary_details = api.model('BursaryDetails', {
+    'name': fields.String(required=True, description='Name of the bursary'),
+})
+
+@api.route('/add_bursary')
 class AddBursary(Resource):
+    @api.doc('add_bursary')
+    @api.expect(bursary_details)
     def post(self):
         schema = BursarySchema()
         try:
@@ -19,14 +27,19 @@ class AddBursary(Resource):
         db.session.commit()
         return {"message": "Bursary added successfully."}, 201
 
+@api.route('/view_applications')
 class ViewApplications(Resource):
+    @api.doc('view_applications')
+    @api.marshal_list_with(bursary_details)
     def get(self):
         schema = BursarySchema(many=True)
         applications = Bursary.query.all()
         result = schema.dump(applications)
         return result, 200
 
+@api.route('/award_bursary/<application_id>')
 class AwardBursary(Resource):
+    @api.doc('award_bursary')
     def post(self, application_id):
         application = Bursary.query.get(application_id)
         application_id = uuid.UUID(application_id)
@@ -36,14 +49,18 @@ class AwardBursary(Resource):
             return {"message": "Bursary awarded successfully."}, 200
         return {"message": "Application not found."}, 404
 
+@api.route('/view_students')
 class ViewStudents(Resource):
+    @api.doc('view_students')
     def get(self):
         schema = StudentDetailsSchema(many=True)
         students = StudentDetails.query.all()
         result = schema.dump(students)
         return result, 200
 
+@api.route('/reject_request/<application_id>')
 class RejectRequest(Resource):
+    @api.doc('reject_request')
     def post(self, application_id):
         application = Bursary.query.get(application_id)
         application_id = uuid.UUID(application_id)
