798e5252eb9492eca942a53aa6437bfcf84864d8
arronkiptoo <kiptooaarron@gmail.com>
2024-02-25 22:03:02 +0300

    added landing page

---
+++ clients/src/components/MainContent/MainContent.jsx
@@ -0,0 +1,24 @@
+// MainContent.js
+import React from 'react';
+import './mainContent.css';
+import mainImage from '../../assets/images/image.png';
+
+function MainContent() {
+    return (
+        <main className="main-content">
+            <div className="text-content">
+                <h1>HERE TO EMPOWER YOUR DREAMS</h1>
+                <p>Development Bank of Kenya through its Education financing department intervenes through education and infrastructure financing and efficient management of student loan and bursary schemes for tertiary education within and outside of Kenya.</p>
+                <div className="buttons">
+                    <button className="btn get-started">Get Started</button>
+                    <button className="btn login">Login</button>
+                </div>
+            </div>
+            <div className="image-content">
+                <img src={mainImage} alt="Empowered Student" />
+            </div>
+        </main>
+    );
+}
+
+export default MainContent;
